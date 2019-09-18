ee9b4662f791a931c4daf779b2e45e73ed456fb7
Mao Yufeng <lcdsmao@gmail.com>
2019-09-18 13:09:08 +0900

    Avoid pre draw multiple times

--- lib/src/main/java/jp/co/cyberagent/android/tabanimation/ViewAnimationInfo.kt
+++ lib/src/main/java/jp/co/cyberagent/android/tabanimation/ViewAnimationInfo.kt
@@ -6,6 +6,7 @@ import android.animation.PropertyValuesHolder
 import android.animation.TimeInterpolator
 import android.os.Build
 import android.view.View
+import androidx.core.view.OneShotPreDrawListener
 import androidx.core.view.ViewCompat
 import androidx.core.view.doOnPreDraw
 import java.lang.ref.WeakReference
@@ -25,6 +26,7 @@ internal class ViewAnimationInfo<V : View, T>(
   private val animator by lazy(LazyThreadSafetyMode.NONE) {
     createAnimator()
   }
+  private var preDrawListener: OneShotPreDrawListener? = null
 
   fun updateProperty(fraction: Float) {
     root.get()?.ensureLaidOut {
@@ -46,10 +48,11 @@ internal class ViewAnimationInfo<V : View, T>(
   }
 
   private fun View.ensureLaidOut(action: () -> Unit) {
-    if (ViewCompat.isLaidOut(this)) {
+    preDrawListener?.removeListener()
+    if (ViewCompat.isLaidOut(this) && !isLayoutRequested) {
       action()
     } else {
-      doOnPreDraw { action() }
+      preDrawListener = doOnPreDraw { action() }
     }
   }
 
